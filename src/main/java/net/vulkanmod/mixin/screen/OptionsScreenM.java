dd18b18efa0d224e6a12e6b15cfeea8bb2a82629
PedrixzZ <131414300+PedrixzZ@users.noreply.github.com>
2024-06-15 10:59:37 -0300

    Update OptionsScreenM.java

--- src/main/java/net/vulkanmod/mixin/screen/OptionsScreenM.java
+++ src/main/java/net/vulkanmod/mixin/screen/OptionsScreenM.java
@@ -3,10 +3,9 @@ package net.vulkanmod.mixin.screen;
 import com.mojang.blaze3d.platform.GlStateManager; // Import for managing OpenGL state
 import com.mojang.blaze3d.systems.RenderSystem; // Import for rendering
 import net.minecraft.client.Options;
-import net.minecraft.client.gui.screen.option.GameOptionsScreen;
+import net.minecraft.client.gui.screen.option.VideoOptionsScreen;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.network.chat.Component;
-import net.minecraft.util.text.StringTextComponent; // Use StringTextComponent for text
 import net.vulkanmod.config.gui.VOptionScreen; // Assuming VOptionScreen is your custom video options screen
 
 import org.spongepowered.asm.mixin.Final;
@@ -16,7 +15,7 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(GameOptionsScreen.class) // Use AbstractOptionsScreen for 1.21
+@Mixin(VideoOptionsScreen.class) // Use AbstractOptionsScreen for 1.21
 public class OptionsScreenM extends Screen {
 
     @Shadow @Final private Screen lastScreen; // Shadow the lastScreen field
